29e69a27baf2e4799e545aa799d0af6e31e94736
kaller_axe <ashirbaev010399@gmail.com>
2023-02-09 17:03:18 +0200

    Add Assert Method in WebElement, Add Category, CreateCategoryClass, Add Object in BasicUiTest

--- src/test/java/uiTests/BaseUiTest.java
+++ src/test/java/uiTests/BaseUiTest.java
@@ -3,14 +3,17 @@ package uiTests;
 import com.talentLMS.UI.dataProviders.ConfigReader;
 import com.talentLMS.UI.driverFactory.Driver;
 import com.talentLMS.UI.helper.WebElementActions;
+import com.talentLMS.UI.page.сategory.CategoryPage;
 import com.talentLMS.UI.page.LoginPage;
+import com.talentLMS.UI.page.AdminHomePage;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public abstract class BaseUiTest {
-
     public LoginPage loginPage;
+    public AdminHomePage adminHomePage;
+    public CategoryPage categoryPage;
     public WebElementActions webElementActions;
     public WebDriver driver;
 
@@ -18,6 +21,8 @@ public abstract class BaseUiTest {
     public void setUpUiTest() {
         loginPage = new LoginPage();
         webElementActions = new WebElementActions();
+        adminHomePage = new AdminHomePage();
+        categoryPage = new CategoryPage();
         driver = Driver.getDriver();
         driver.navigate().to(ConfigReader.getProperty("qa_env"));
         loginPage.fillUpUsernameAndPassword();
