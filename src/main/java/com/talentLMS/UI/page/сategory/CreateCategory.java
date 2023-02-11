162800f3232d40eaedb6d0bdac4f8db3b5f90ba7
kaller_axe <ashirbaev010399@gmail.com>
2023-02-11 09:00:02 +0200

    My Push Farrukh With Category

--- src/main/java/com/talentLMS/UI/page/сategory/CreateCategory.java
+++ src/main/java/com/talentLMS/UI/page/сategory/CreateCategory.java
@@ -1,9 +1,40 @@
 package com.talentLMS.UI.page.сategory;
 
 import com.talentLMS.UI.page.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 /**
  * @author Farrukh Ashirbaev
  */
 public class CreateCategory extends BasePage {
 
+    @FindBy(xpath = "//div[@class='tl-title tl-ellipsis']")
+    private WebElement addCategoryTitleInHeader;
+
+    @FindBy(xpath ="//label[@class='control-label tl-mandatory-label']")
+    private WebElement nameTitleForField;
+
+    @FindBy(xpath = "//label[@for='s2id_autogen1']")
+    private WebElement parentCategoryTitleForField;
+
+    @FindBy(xpath = "//a[@id='show-price']")
+    private WebElement priceTitle;
+
+    @FindBy(xpath = "//i[@id='icon-price']")
+    private WebElement priceIcon;
+
+    @FindBy(xpath = "//input[@name='name']")
+    private WebElement nameField;
+
+    @FindBy(xpath = "//span[@class='select2-arrow']/b")
+    private WebElement parentCategoryDDL;
+
+    @FindBy(xpath = "//input[@name='price']")
+    private WebElement priceField;
+
+    @FindBy(xpath = "//ul[@class='select2-results']/li/div")
+    private List <WebElement> categoryParentListDDL;
 }
---
+++ src/test/java/uiTests/category/CreateCategory.java
@@ -0,0 +1,7 @@
+package uiTests.category;
+
+import com.talentLMS.UI.page.BasePage;
+
+public class CreateCategory extends BasePage {
+
+}
