c57584b01904d10a52fcec522fb5b56c52d6dbf7
Aidai <zumabekovnaajdaj29@gmail.com>
2023-02-14 13:47:46 +0300

    added assertions

--- src/test/java/uiTests/branches/AddBranchTest.java
+++ src/test/java/uiTests/branches/AddBranchTest.java
@@ -1,5 +1,6 @@
 package uiTests.branches;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import uiTests.BaseUiTest;
 
@@ -7,5 +8,6 @@ public class AddBranchTest extends BaseUiTest {
     @Test
     public void clickAddUserBtn(){
         branchesPage.clickAddBranchBtn();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://nuta1bema.talentlms.com/branch/index");
     }
 }
