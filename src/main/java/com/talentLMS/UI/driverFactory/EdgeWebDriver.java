e8fc8a7cc7d75453c40d0ca759b631d96c6c85d3
Azat33 <123890963+Azat33@users.noreply.github.com>
2023-03-10 16:55:28 +0600

    Update EdgeWebDriver.java

--- src/main/java/com/talentLMS/UI/driverFactory/EdgeWebDriver.java
+++ src/main/java/com/talentLMS/UI/driverFactory/EdgeWebDriver.java
@@ -1,8 +1,10 @@
 package com.talentLMS.UI.driverFactory;
 
+import com.talentLMS.UI.dataProviders.ConfigReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 import java.time.Duration;
 /**
@@ -16,6 +18,11 @@ public class EdgeWebDriver {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().deleteAllCookies();
+        
+         EdgeOptions options = new EdgeOptions();
+        if (Boolean.parseBoolean(ConfigReader.getProperty("headless"))){
+            options.addArguments("--headless");
+        }
 
         return driver;
     }
